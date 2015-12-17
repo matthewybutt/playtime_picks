fa01d1d8aca89c2192cf353353f5bad3d1a45298
Matthew Butt <matthew.y.butt@gmail.com>
2015-12-17 11:27:27 -0800

    Move add activity to own route, style other routes

--- public/app.routes.js
+++ public/app.routes.js
@@ -12,8 +12,8 @@
     $stateProvider
       .state("home", {
         url: "/",
-        templateUrl: "/templates/home.html",
-        controller: "MainController",
+        templateUrl: "/templates/activities.html",
+        controller: "ActivitiesController",
         controllerAs: "vm"
       })
       .state("activities", {
@@ -25,6 +25,12 @@
       .state("activity", {
         url: "/activities/{id}",
         templateUrl: "/templates/activity.html",
+        controller: "ActivitiesController",
+        controllerAs: "vm"
+      })
+      .state("addActivity", {
+        url: "/add_activty",
+        templateUrl: "/templates/add_activity.html",
         controller: "ActivitiesController",
         controllerAs: "vm"
       })
