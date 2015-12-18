3393dd62ac76ff07d30ee05cb52128ec84980e6a
Matthew Butt <matthew.y.butt@gmail.com>
2015-12-18 03:22:30 -0800

    Reconfig file structure for heroku deploy

--- app/config/database.js
+++ app/config/database.js
@@ -1,20 +1,20 @@
 var mongoose = require('mongoose');
 
-var env = require('./environment');
+// var env = require('./environment');
 
 // Use different database URIs based on whether an env var exists.
 
-var dbUri = process.env.MONGOLAB_URI
+var dbUri = process.env.MONGOLAB_URI;
 // ||
 //             'mongodb://localhost/' + env.localEnvVars.SAFE_TITLE;
 
-if (!env.MONGOLAB_URI) {
-  // check that MongoD is running...
-  require('net').connect(27017, 'localhost').on('error', function() {
-    console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
-    process.exit(0);
-  });
-}
+// if (!env.MONGOLAB_URI) {
+//   // check that MongoD is running...
+//   require('net').connect(27017, 'localhost').on('error', function() {
+//     console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
+//     process.exit(0);
+//   });
+// }
 
 mongoose.connect(dbUri, function(){
   console.log("Connected to db: " + dbUri);
